e7ce2f5d48bd0d5c38b39827c62a979381e370c2
Vmounika-06 <mveeranki8@gmail.com>
2025-11-23 23:03:39 +0530

    Update success message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Automated Deployment Successful! 🚀");
+    res.send("DevOps automation project working! 🚀");
 });
 
 app.listen(3000, () => {
